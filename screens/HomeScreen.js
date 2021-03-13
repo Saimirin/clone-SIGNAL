483187a51880e4ef722bb1a5cc84a1559510c6e7
meilun  difrancisco <ellen0816@hotmail.com>
2021-03-13 18:38:57 -0500

    added signout feature to HomeScreen comp

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,17 @@
 import React, { useLayoutEffect } from 'react'
-import { SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native'
+import { SafeAreaView, ScrollView, StyleSheet, Text, View, TouchableOpacity } from 'react-native'
 import { Avatar } from 'react-native-elements/dist/avatar/Avatar'
 import CustomListItem from "../components/CustomListItem"
 import { auth, db } from "../firebase"
 
 
 const HomeScreen = ({ navigation }) => {
+
+    const signOutUser = () => {
+        auth.signOut().then(()=>{
+            navigation.replace("Login")
+        })
+    }
 
     useLayoutEffect (()=>{
         navigation.setOptions({
@@ -15,7 +21,10 @@ const HomeScreen = ({ navigation }) => {
             headerTintColor: "black",
             headerLeft: ()=> (
             <View style={{ marginLeft: 20 }}>
-                <Avatar rounded source={{ uri: auth?.currentUser?.photoURL }} />
+                <TouchableOpacity onPress={signOutUser} activeOpacity={0.5}>
+                    <Avatar rounded source={{ uri: auth?.currentUser?.photoURL }} />
+                </TouchableOpacity>
+                
             </View>
             )
         })
