23af0fb0a5b4a8a3b0a68a8ede4df996d43d4438
meilun  difrancisco <ellen0816@hotmail.com>
2021-03-12 21:58:00 -0500

    created LoginScreen component

---
+++ firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDZF3o_CeXZK2MApwaVWEAmyxCIyK9lezY",
+    authDomain: "clone-signal-6a84d.firebaseapp.com",
+    projectId: "clone-signal-6a84d",
+    storageBucket: "clone-signal-6a84d.appspot.com",
+    messagingSenderId: "276329955804",
+    appId: "1:276329955804:web:2bff487e1f3797aebb8f19",
+    measurementId: "G-XFDJRFWBKH"
+  };
