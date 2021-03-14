5f90ad0f0bb4c1def973b1646793566e230d2605
meilun  difrancisco <ellen0816@hotmail.com>
2021-03-13 20:10:25 -0500

    edit the HomeScreen comp, pull chat list from firebase and map each of them to CustomListItem comp

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import React, { useLayoutEffect } from 'react'
+import React, { useEffect, useLayoutEffect, useState } from 'react'
 import { SafeAreaView, ScrollView, StyleSheet, Text, View, TouchableOpacity } from 'react-native'
 import { Avatar } from 'react-native-elements/dist/avatar/Avatar'
 import CustomListItem from "../components/CustomListItem"
@@ -6,14 +6,25 @@ import { auth, db } from "../firebase"
 import { AntDesign, SimpleLineIcons } from "@expo/vector-icons"
 
 const HomeScreen = ({ navigation }) => {
+    const [chats, setChats] = useState([])
 
-    
 
     const signOutUser = () => {
         auth.signOut().then(()=>{
             navigation.replace("Login")
         })
     }
+
+    useEffect(()=>{
+        const unsubscribe = db. collection('chats').onSnapshot(snapshot => (
+            setChats (snapshot.docs.map(doc => ({
+                id: doc.id,
+                data: doc.data ()
+            })))
+        ))
+
+        return unsubscribe
+    },[])
 
     useLayoutEffect (()=>{
         navigation.setOptions({
@@ -51,6 +62,9 @@ const HomeScreen = ({ navigation }) => {
     return (
         <SafeAreaView>
             <ScrollView>
+                {chats.map(({id, data: {chatName}}) => (
+                    <CustomListItem key={id} id={id} chatName={chatName}/>
+                ))}
                 <CustomListItem />
             </ScrollView>
         </SafeAreaView>
