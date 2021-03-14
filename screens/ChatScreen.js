dbef082469bd8f625de03d27afc1a786e74b8f16
meilun  difrancisco <ellen0816@hotmail.com>
2021-03-13 21:08:35 -0500

    added more style to ChatScreen render content

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -2,6 +2,11 @@ import React, { useLayoutEffect } from 'react'
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
 import { ListItem, Avatar } from "react-native-elements"
 import { AntDesign, FontAwesome, Ionicons } from "@expo/vector-icons"
+import { SafeAreaView } from 'react-native'
+import { StatusBar } from 'expo-status-bar'
+import { KeyboardAvoidingView } from 'react-native'
+import { Platform } from 'react-native'
+import { ScrollView, TextInput } from 'react-native-gesture-handler'
 
 const ChatScreen = ({ navigation, route }) => {
     useLayoutEffect (() => {
@@ -63,14 +68,30 @@ const ChatScreen = ({ navigation, route }) => {
     },[navigation])
 
     return (
-        <View>
-            <Text>
-                {route.params.chatName}
-            </Text>
-        </View>
+        <SafeAreaView style={{ flex:1, backgroundColor: "white" }}>
+            <StatusBar style="light" />
+
+            <KeyboardAvoidingView
+                 behavior={Platform.OS === "ios" ? "padding" : "height" }
+                 style={styles.container}
+                 keyboardVerticalOffset={90}
+            >
+                <>
+                    <ScrollView>
+                        {/* {Chat goes here} */}
+                    </ScrollView>
+                    <View style={styles.footer}>
+                        <TextInput placeholder="Signal Message" />
+                    </View>
+                </>
+            </KeyboardAvoidingView>
+        </SafeAreaView>
     )
 }
 
 export default ChatScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {},
+    footer: {}
+})
